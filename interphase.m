d9d2d5ed1733d6193593bcb72a5388002eddf864
Ben Mullet <bmullet@stanford.edu>
2020-05-01 09:50:46 -0700

    add interphase.m for calculating interphase terms

---
+++ interphase.m
@@ -0,0 +1,36 @@
+    function Fmg = interphase(duv, phiv, rhogv,A)
+        Fmg = nan(size(duv));
+        
+            for n = 1:length(duv)
+                du = duv(n);
+                phi = phiv(n);
+                rhog = rhogv(n);
+                
+                rb = (3*phi/(4*pi*A.nb))^(1/3); %bubble radius
+                rbnd = rb/A.C.rb0;
+                
+                if (A.useForchheimer)
+                    k1 = (phi/A.phi0)^(A.m+2/3);
+                    k2 = (phi/A.phi0)^((1+3*A.m)/2+1/3);
+                    Fmg1 = -1/A.C.St*(1+A.C.Fo*k1/k2*abs(du)*rhog)*phi*(1-phi)/k1*du;
+                    
+                else
+                    Fmg1 = -1/A.C.Reb * A.C.rc/A.C.rb0 * 1/rbnd^2 * (9/2*phi*(1-phi)*A.mu(phi,p*A.C.p0)/A.C.mu0*(du));
+                end
+                
+                
+                Fmg2 = -3/8*phi*(1-phi)*A.dragC*A.C.rc/A.Rash*rhog*abs(du)*(du)*A.C.delta;
+                
+                pf = A.phiforce;
+                
+                if phi<A.phi0
+                    Fmg(n) = Fmg1;
+                elseif (phi>=A.phi0) && (phi<pf)
+                    t = (phi-A.phi0)/(pf-A.phi0);
+                    Fmg(n) = -1*(abs(Fmg1))^(1-t)*(abs(Fmg2))^(t)*sign(du);
+                elseif phi>=pf
+                    Fmg(n) = Fmg2;
+                else
+                    Fmg(n)=0;
+                end
+            end
